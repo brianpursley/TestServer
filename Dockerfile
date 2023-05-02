b61b853d4ffd48dfa6e7ecf6dd3ebeef13561e12
Brian Pursley <bpursley@cinlogic.com>
2023-05-01 21:46:52 -0400

    Added project files

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:7.0 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:7.0 AS build
+WORKDIR /src
+COPY ["TestTcpServer/TestTcpServer.csproj", "TestTcpServer/"]
+RUN dotnet restore "TestTcpServer/TestTcpServer.csproj"
+COPY . .
+WORKDIR "/src/TestTcpServer"
+RUN dotnet build "TestTcpServer.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "TestTcpServer.csproj" -c Release -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "TestTcpServer.dll"]
---
+++ TestTcpServer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var serverAddress = IPAddress.Parse("127.0.0.1");
+const int serverPort = 5000;
+
+var server = new TcpListener(serverAddress, serverPort);
+server.Start();
+Console.WriteLine($"Server started on {serverAddress}:{serverPort}");
+
+var i = 0;
+while (true)
+{
+    var connectionNumber = Interlocked.Increment(ref i);
+
+    var client = server.AcceptTcpClient();
+    Console.WriteLine($"[{connectionNumber}] Connected to client at {client.Client.RemoteEndPoint}");
+
+    var clientThread = new Thread(clientObj =>
+    {
+        if (clientObj == null)
+        {
+            return;
+        }
+        var connection = (TcpClient)clientObj;
+        var stream = connection.GetStream();
+        var buffer = new byte[1024];
+        while (true)
+        {
+            var bytesRead = stream.Read(buffer, 0, buffer.Length);
+            if (bytesRead == 0)
+            {
+                Console.WriteLine($"[{connectionNumber}] Connection was closed by {connection.Client.RemoteEndPoint}");
+                break;
+            }
+            if (Encoding.ASCII.GetString(buffer, 0, bytesRead) == "\\test\n")
+            {
+                Console.WriteLine($"[{connectionNumber}] Test command received. Closing connection to {connection.Client.RemoteEndPoint}");
+                break;
+            }
+            var dateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            Console.WriteLine($"[{connectionNumber}] Received {bytesRead} bytes from {connection.Client.RemoteEndPoint} at {dateTime}");
+            stream.Write(Encoding.ASCII.GetBytes($"n={bytesRead}, t={dateTime}\n"));
+        }
+        stream.Close();
+        connection.Close();
+    });
+    
+    clientThread.Start(client);
+}
